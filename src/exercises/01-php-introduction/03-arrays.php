5cbe6cea3b2d6ebf36dd0802aca20f1faf88152e
Lennon Graham <n00254168@iadt.ie>
2026-01-20 14:02:51 +0000

    arrays excercises completed

--- src/exercises/01-php-introduction/03-arrays.php
+++ src/exercises/01-php-introduction/03-arrays.php
@@ -27,6 +27,12 @@
     <div class="output">
         <?php
         // TODO: Write your solution here
+        $movies = ['star wars', 'jurrasic park', 'avengers', 'toy story', 'cars'];
+        echo "<ul>";
+        for ($i = 0; $i < count($movies); $i++) {
+            echo "<li>Movie $i: $movies[$i]</li>";
+        }
+        echo "</ul>";
         ?>
     </div>
 
@@ -42,6 +48,15 @@
     <div class="output">
         <?php
         // TODO: Write your solution here
+        $student = [
+            "name" => "Michael Johnson",
+            "studentId" => "0123456789",
+            "course" => "computers",
+            "grade" => "F"
+        ];
+
+        $text = "{$student['name']} with student ID number {$student['studentId']} in the {$student['course']} course has the grade {$student['grade']}";
+        print("<p>$text</p>")
         ?>
     </div>
 
@@ -58,6 +73,18 @@
     <div class="output">
         <?php
         // TODO: Write your solution here
+        $capitals = [
+            "Ireland" => "Dublin",
+            "England" => "London",
+            "Scotland" => "Glasgow",
+            "Wales" => "Cardiff",
+            "America" => "Washington DC"
+        ];
+        echo "<ul>";
+        foreach ($capitals as $country => $capital) {
+            echo "<li>$capital is the capital of $country</li>";
+        }
+        echo "</ul>"
         ?>
     </div>
 
@@ -75,6 +102,31 @@
     <div class="output">
         <?php
         // TODO: Write your solution here
+        $menu = [
+            'Starters' => [
+                'Soup of the day' => "7.50",
+                'BBQ Ribs' => "8.50",
+                'Salad' => "7.00" 
+            ],
+            'Main course' => [
+                'Pizza' => "12.50",
+                'Chicken Pasta' => "13.00",
+                'Beef Burger' => "13.50"
+            ],
+            'Dessert' => [
+                'Brownie' => "9.00",
+                'Ice Cream' => "6.00",
+                'Apple Pie' => "8.00"
+            ]
+        ];
+        foreach ($menu as $category => $item) {
+            echo "<p>" . ucfirst($category) . "</p>";
+            echo "<ul>";
+            foreach ($item as $prices => $price) {
+                echo "<li>$prices\t €$price</li>";
+            }
+            echo "</ul>";
+        }
         ?>
     </div>
 
