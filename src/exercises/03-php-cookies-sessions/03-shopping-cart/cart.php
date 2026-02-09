ed54c599885ec752f80d0b4481aafeb98572a5bb
Lennon Graham <n00254168@iadt.ie>
2026-02-09 10:36:58 +0000

    finished the cart excercises

--- src/exercises/03-php-cookies-sessions/03-shopping-cart/cart.php
+++ src/exercises/03-php-cookies-sessions/03-shopping-cart/cart.php
@@ -33,10 +33,7 @@ $cart = ShoppingCart::getInstance();
 // TODO: Write your code here
 if (isset($_GET['remove'])) {
     $id = (int)$_GET['remove'];
-    $product = Product::findById($id);
-    if ($product !== null) {
-        $cart->remove($id);
-    }
+    $cart->remove($id);
     header("Location: cart.php");
     exit();
 }
@@ -49,7 +46,12 @@ if (isset($_GET['remove'])) {
 // 2. Redirect back to cart.php
 // -----------------------------------------------------------------------------
 // TODO: Write your code here
-
+if (isset($_GET['clear'])) {
+    $id = (int)$_GET['clear'];
+    $cart->clear($id);
+    header("Location: cart.php");
+    exit();
+}
 // =============================================================================
 
 // =============================================================================
