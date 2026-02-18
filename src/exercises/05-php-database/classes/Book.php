1031f8afea9951a71b277f4a5f99ec67cf9644f2
Lennon Graham <n00254168@iadt.ie>
2026-02-18 11:31:46 +0000

    08 model basics excercises complete

--- src/exercises/05-php-database/classes/Book.php
+++ src/exercises/05-php-database/classes/Book.php
@@ -45,6 +45,16 @@ class Book
     {
         // TODO: Get database connection from DB singleton
         // TODO: If $data is not empty, populate properties using null coalescing operator
+        $this->db = DB::getInstance()->getConnection();
+
+        $this->id             = $data['id'] ?? null;
+        $this->title          = $data['title'] ?? null;
+        $this->author         = $data['author'] ?? null;
+        $this->publisher_id   = $data['publisher_id'] ?? null;
+        $this->year           = $data['year'] ?? null;
+        $this->isbn           = $data['isbn'] ?? null;
+        $this->description    = $data['description'] ?? null;
+        $this->cover_filename = $data['cover_filename'] ?? null;
     }
 
     // =========================================================================
@@ -93,5 +103,15 @@ class Book
     public function toArray()
     {
         // TODO: Implement this method
+            return [
+            'id'             => $this->id,
+            'title'          => $this->title,
+            'author'         => $this->author,
+            'publisher_id'   => $this->publisher_id,
+            'year'           => $this->year,
+            'isbn'           => $this->isbn,
+            'description'    => $this->description,
+            'cover_filename' => $this->cover_filename
+        ];
     }
 }
