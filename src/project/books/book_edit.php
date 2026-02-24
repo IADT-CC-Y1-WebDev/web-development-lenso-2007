b87d036a4e87f11915acf5525ded834d60ef09cb
Lennon Graham <n00254168@iadt.ie>
2026-02-24 14:02:00 +0000

    book update almost finished

---
+++ src/project/books/book_edit.php
@@ -0,0 +1,129 @@
+<?php
+require_once 'php/lib/config.php';
+require_once 'php/lib/session.php';
+require_once 'php/lib/forms.php';
+require_once 'php/lib/utils.php';
+
+startSession();
+
+try {
+    if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+        throw new Exception('Invalid request method.');
+    }
+    if (!array_key_exists('id', $_GET)) {
+        throw new Exception('No book ID provided.');
+    }
+    $id = $_GET['id'];
+
+    $book = Book::findById($id);
+    if ($book === null) {
+        throw new Exception("Book not found.");
+    }
+
+    $bookFormats = Format::findByBookId($book->id);
+    $bookFormatsIds = [];
+    foreach ($bookFormats as $format) {
+        $bookFormatsIds[] = $format->id;
+    }
+
+    $publishers = Publisher::findAll();
+    $formats = Format::findAll();
+}
+catch (PDOException $e) {
+    setFlashMessage('error', 'Error: ' . $e->getMessage());
+    redirect('/index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include 'php/inc/head_content.php'; ?>
+        <title>Edit Book</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="width-12">
+                <?php require 'php/inc/flash_message.php'; ?>
+            </div>
+            <div class="width-12">
+                <h1>Edit Book</h1>
+            </div>
+            <div class="width-12">
+                <form action="book_update.php" method="POST" enctype="multipart/form-data">
+                    <div class="input">
+                        <input type="hidden" name="id" value="<?= h($book->id) ?>">
+                    </div>
+                    <div class="input">
+                        <label class="special" for="title">Title:</label>
+                        <div>
+                            <input type="text" id="title" name="title" value="<?= old('title', $book->title) ?>" required>
+                            <p><?= error('title') ?></p>
+                        </div>
+                    </div>
+                    <div class="input">
+                        <label class="special" for="release_date">Release Year:</label>
+                        <div>
+                            <input type="number" id="release_date" name="release_date" value="<?= old('release_date', $book->release_date) ?>" required>
+                            <p><?= error('release_date') ?></p>
+                        </div>
+                    </div>
+                    <div class="input">
+                        <label class="special" for="publisher_id">Publisher:</label>
+                        <div>
+                            <select id="publisher_id" name="publisher_id" required>
+                                <?php foreach ($publishers as $publisher) { ?>
+                                    <option value="<?= h($publisher->id) ?>" <?= chosen('publisher_id', $publisher->id, $book->publisher_id) ? "selected" : "" ?>>
+                                        <?= h($publisher->name) ?>
+                                    </option>
+                                <?php } ?>
+                            </select>
+                            <p><?= error('publisher_id') ?></p>
+                        </div>
+                    </div>
+                    <div class="input">
+                        <label class="special" for="description">Description:</label>
+                        <div>
+                            <textarea id="description" name="description" required><?= old('description', $book->description) ?></textarea>
+                            <p><?= error('description') ?></p>
+                        </div>
+                    </div>
+                    <div class="input">
+                        <label class="special">Formats:</label>
+                        <div>
+                            <?php foreach ($formats as $format) { ?>
+                                <div>
+                                    <input type="checkbox" 
+                                        id="format_<?= h($format->id) ?>" 
+                                        name="format_ids[]" 
+                                        value="<?= h($format->id) ?>"
+                                        <?= chosen('format_ids', $format->id, $bookFormatsIds) ? "checked" : "" ?>
+                                    >
+                                    <label for="format_<?= h($format->id) ?>"><?= h($format->name) ?></label>
+                                </div>
+                            <?php } ?>
+                        </div>
+                        <p><?= error('format_ids') ?></p>
+                    </div>
+                    <div><img src="images/<?= $book->image_filename ?>" /></div>
+                    <div class="input">
+                        <label class="special" for="image">Image (optional):</label>
+                        <div>
+                            <input type="file" id="image" name="image" accept="image/*">
+                            <p><?= error('image') ?></p>
+                        </div>
+                    </div>
+                    <div class="input">
+                        <button class="button" type="submit">Update Book</button>
+                        <div class="button"><a href="index.php">Cancel</a></div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
+<?php
+// Clear form data after displaying
+clearFormData();
+// Clear errors after displaying
+clearFormErrors();
+?>
